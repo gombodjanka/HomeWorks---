525a36700e702c604ba1ef3581855210b4949197
Aryana <gombodjan1512@mail.ru>
2024-08-17 09:20:44 +0300

    added homework on the topic introduction to react

---
+++ REACT/my-app/src/App.js
@@ -0,0 +1,45 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  let menu = [
+    {
+      name: "Спагетти Болоньезе",
+      ingredients: ["Лук","Оливковое масло","Мясной фарш","Помидоры в собственном соку","Чеснок","Сладкий перец", "Приправы","Спагетти", "Тертый твердый сыр"],
+      price: 650,
+      image: "https://i.pinimg.com/564x/4f/b9/f2/4fb9f2a9698bbb1977c2494b92035a1f.jpg"
+    },
+    {
+        name: "Шакшука",
+        ingredients: ["Лук","Оливковое масло","Помидоры","Яйца","Томатная паста","Чеснок","Сладкий перец","Приправы"],
+        price: 650,
+        image: "https://i.pinimg.com/564x/d7/3b/37/d73b376f079389359325ab94b67d9909.jpg"
+    },
+    {
+        name: "Сырники",
+        ingredients: ["Творог","Мука","Яйца","Сахар","Масло","Соль"],
+        price: 350,
+        image: "https://i.pinimg.com/564x/04/42/58/044258902613794036b47c199225be90.jpg"
+    }
+  ]
+  return (
+    <div className="App">
+      <h1>МЕНЮ</h1>
+      <div className='menu'>   
+      {menu.map (function (item) { 
+        return (
+          <div className='card'>
+            <img src={item.image} alt={item.name}/>
+            <h2>{item.name}</h2>
+            <h3>Ингредиенты: {item.ingredients.join(', ')}</h3>
+            <h4>{item.price}</h4>
+    
+          </div>
+        );  
+      })}
+      </div>
+    </div>
+  );
+}
+
+export default App;
